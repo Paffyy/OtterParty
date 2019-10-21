5e9cd578de49d241f7e3ec1209017744338a2bcc
Patric Wåhlin <pw@teamwahlin.se>
2019-10-21 13:52:09 +0200

    scenes + Checkpoints

---
+++ OtterParty/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private int loadSceneIndex;
+
+    private void OnChangeScene()
+    {
+        SceneManager.LoadScene(loadSceneIndex);
+    }
+}
