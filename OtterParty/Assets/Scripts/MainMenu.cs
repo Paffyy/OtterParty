30fd339187d6f385e932522905c064eb5306314c
taey1 <37865938+taey1@users.noreply.github.com>
2019-10-22 14:09:43 +0200

    Update MainMenu.cs

--- OtterParty/Assets/Scripts/MainMenu.cs
+++ OtterParty/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Debug.Log("PlayGame");
         // todo
     }
 
     public void OpenOptions()
     {
+        Debug.Log("OpenOptions");
         // todo
     }
 
