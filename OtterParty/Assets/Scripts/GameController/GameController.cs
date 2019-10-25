ce1eedaac814f18bd76c4fcd8cf3e478dafbaf53
Paffyy <pw@teamwahlin.se>
2019-10-25 14:03:46 +0200

    GameController fixes, JoinScene

--- OtterParty/Assets/Scripts/GameController/GameController.cs
+++ OtterParty/Assets/Scripts/GameController/GameController.cs
@@ -8,8 +8,8 @@ public class GameController : MonoBehaviour
     public PointSystem PointSystem { get; set; } 
     public List<Player> Players { get; set; } = new List<Player>();
     private List<Minigame> minigames = new List<Minigame>();
-    private Minigame currentMinigame;
-
+    private Minigame nextMinigame;
+    private int nextMinigameIndex = 0;
     #region Singleton
     private GameController() { }
     private static GameController instance;
@@ -26,24 +26,35 @@ public class GameController : MonoBehaviour
 
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
         var parrent = GameObject.Find("Minigames");
         if (parrent != null)
         {
-            foreach (GameObject item in parrent.transform)
+            foreach (Transform item in parrent.transform)
             {
-                var minigame = item.GetComponent<Minigame>();
+                var minigame = item.gameObject.GetComponent<Minigame>();
                 if (minigame != null)
                 {
                     minigames.Add(minigame);
                 }
             }
-            currentMinigame = minigames[0];
+            nextMinigame = minigames[nextMinigameIndex];
         }
     }
     public void StartNextMinigame()
     {
-        SceneManager.LoadScene(currentMinigame.Id);
+        if (nextMinigameIndex < minigames.Count)
+        {
+            nextMinigame = minigames[nextMinigameIndex];
+            SceneManager.LoadScene(nextMinigame.SceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+            nextMinigameIndex = 0;
+            return;
+        }
+        nextMinigameIndex++;
     }
-
 }
 
--- OtterParty/Assets/Scripts/Structs/Minigame.cs
+++ OtterParty/Assets/Scripts/Structs/Minigame.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minigame 
+public class Minigame : MonoBehaviour
 {
-    public int Id { get; set; }
-    public int Name { get; set; }
+    [SerializeField]
+    private int sceneIndex;
+    public int SceneIndex
+    {
+        get { return sceneIndex; }
+        set { sceneIndex = value; }
+    }
 
+    public string Name { get; set; }
+    private void Awake()
+    {
+        Name = gameObject.name;
+    }
 }
