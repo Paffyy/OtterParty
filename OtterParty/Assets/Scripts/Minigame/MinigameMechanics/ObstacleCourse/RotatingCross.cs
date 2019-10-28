0507d47a06d6b8d5270e4d6b70c73b2b9e2452a4
taey1 <37865938+taey1@users.noreply.github.com>
2019-10-28 16:05:55 +0100

    RotatingCross

---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/ObstacleCourse/RotatingCross.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCross : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 2.0f)]
+    private float rotationSpeed;
+    [SerializeField]
+    private Transform rotatingCross;
+    [SerializeField]
+    [Range(-1, 1)]
+    private int rotatingDirection;
+
+    void FixedUpdate()
+    {
+        rotatingCross.Rotate(new Vector3(0, rotatingDirection, 0), rotationSpeed);
+    }
+}
