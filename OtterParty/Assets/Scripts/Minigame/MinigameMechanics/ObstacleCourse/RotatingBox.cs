ae929e14011d1b38c587942cda9fcf0c9bd70b6f
taey1 <37865938+taey1@users.noreply.github.com>
2019-10-28 15:35:12 +0100

    RotatingBox

---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/ObstacleCourse/RotatingBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBox : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 2.0f)]
+    private float rotationSpeed;
+    [SerializeField]
+    private Transform rotatingBox;
+
+    void FixedUpdate()
+    {
+        rotatingBox.Rotate(new Vector3(1, 0, 0), rotationSpeed);
+    }
+}
