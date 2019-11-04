2fdec0e9ab4e373aa0b2d1d2c197ed65c28170ea
taey1 <37865938+taey1@users.noreply.github.com>
2019-11-04 23:44:33 +0100

    shooting projectile

--- OtterParty/Assets/Scripts/Character/Mechanics/ProjectileMove.cs
+++ OtterParty/Assets/Scripts/Character/Mechanics/ProjectileMove.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class ProjectileMove : MonoBehaviour
 {
     [SerializeField]
-    [Range(1f, 20f)]
+    [Range(1f, 40f)]
     private float speed;
+    [SerializeField]
+    private GameObject muzzlePrefab;
+    [SerializeField]
+    private GameObject hitPrefab;
+
+    void Start()
+    {
+        if(muzzlePrefab != null)
+        {
+            var muzzleClone = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            muzzleClone.transform.forward = gameObject.transform.forward;
+            var psMuzzle = muzzleClone.GetComponent<ParticleSystem>();
+            if(psMuzzle != null)
+            {
+                Destroy(muzzleClone, psMuzzle.main.duration);
+            }
+            else
+            {
+                var psChild = muzzleClone.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleClone, psChild.main.duration);
+            }
+        }
+    }
 
     void FixedUpdate()
     {
@@ -14,5 +37,28 @@ public class ProjectileMove : MonoBehaviour
         {
             transform.position += transform.forward * (speed * Time.deltaTime);
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 position = contact.point;
+        if(hitPrefab != null)
+        {
+            var hitClone = Instantiate(hitPrefab, position, rotation);
+            var psHit = hitClone.GetComponent<ParticleSystem>();
+            if (psHit != null)
+            {
+                Destroy(hitClone, psHit.main.duration);
+            }
+            else
+            {
+                var psChild = hitClone.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(hitClone, psChild.main.duration);
+            }
+        }
+        Destroy(gameObject);
     }
 }
