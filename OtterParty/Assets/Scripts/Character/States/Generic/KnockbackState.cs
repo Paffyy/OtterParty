a92a74ff6847487a407f40faf29d6a89f9411b50
taey1 <37865938+taey1@users.noreply.github.com>
2019-11-14 02:57:53 +0100

    Revert "shoooove"
    This reverts commit a285e057fca7bd4a2dadb1852aa7b9efed866f8e.

--- OtterParty/Assets/Scripts/Character/States/Generic/KnockbackState.cs
+++ OtterParty/Assets/Scripts/Character/States/Generic/KnockbackState.cs
@@ -9,25 +9,15 @@ public class KnockbackState : CharacterBaseState
     [SerializeField]
     [Range(0.5f,2.0f)]
     private float stunDuration;
-    [SerializeField]
-    [Range(1,10)]
-    private float knockbackMagnitude;
-    public bool IsKnockedBacked { get; set; }
     public override void Enter()
     {
-        IsKnockedBacked = false;
         owner.InputDirection = Vector2.zero;
-        owner.GetComponent<Rigidbody>().velocity = -owner.transform.forward * knockbackMagnitude;
+        owner.GetComponent<Rigidbody>().velocity = -owner.transform.forward * Constants.Instance.DefaultKnockbackDistance;
         base.Enter();
-        Cooldown.Instance.StartNewCooldown(stunDuration, this);
-    }
-    public override void HandleUpdate()
-    {
-        base.HandleUpdate();
-        if (IsKnockedBacked)
+        Task.Factory.StartNew(async() => 
         {
+            await Task.Delay((int)(stunDuration * 1000));
             owner.Transition<MovingState>();
-        }
-
+        });
     }
 }
