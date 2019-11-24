e69892dfc775d45f313ec55a6deef947f2d084c2
taey1 <ybrgus@gmail.com>
2019-11-24 23:59:43 +0100

    hat stuff

--- OtterParty/Assets/Scripts/PlayerHat.cs
+++ OtterParty/Assets/Scripts/PlayerHat.cs
@@ -10,9 +10,24 @@ public class PlayerHat : MonoBehaviour
     private Sprite unavailableHatSprite;
     [SerializeField]
     private int hatIndex;
+    [SerializeField]
+    private Vector3 hatOffset;
+    [SerializeField]
+    private List<Material> hatMaterials;
+    [SerializeField]
+    private MeshRenderer meshRen;
+    [SerializeField]
+    private Vector3 hatRotation;
 
+    public Vector3 HatRotation { get { return hatRotation; } }
+    public Vector3 HatOffset { get { return hatOffset; } }
     public int HatIndex { get { return hatIndex; } }
     public bool IsAvailable { get; set; } = true;
     public Sprite HatSprite { get { return hatSprite; } }
     public Sprite UnavailableHatSprite { get { return unavailableHatSprite; } }
+
+    public void SetPlayerMaterial(int index)
+    {
+        meshRen.material = hatMaterials[index];
+    }
 }
