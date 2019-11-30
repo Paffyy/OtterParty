31f181ac41dffb8110e8450044bd6a24eab1d284
taey1 <ybrgus@gmail.com>
2019-11-30 02:52:24 +0100

    ChickenArenaPrep

---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/ChickenRevenge/ChickenArenaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenArenaController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject chickenBoss;
+    [SerializeField]
+    private List<Transform> chargePoints;
+    [SerializeField]
+
+    void Start()
+    {
+        EventHandler.Instance.Register(EventHandler.EventType.StartMinigameEvent, StartGame);
+    }
+
+    private void StartGame(BaseEventInfo e)
+    {
+       GameObject chicken = Instantiate(chickenBoss);
+       StartCoroutine("ChickenChargeGameLoop");
+    }
+
+    private void RandomizeChargePoints()
+    {
+
+    }
+
+    //IEnumerator ChickenChargeGameLoop()
+    //{
+
+    //}
+}
---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/ChickenRevenge/ChickenBoss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenBoss : MonoBehaviour
+{
+    private Rigidbody chickenBody;
+    void Start()
+    {
+        chickenBody = GetComponent<Rigidbody>();
+       //spawn in animation
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChargeToTarget(List<Vector3> chargePositions, float timeBetweenCharges)
+    {
+        foreach (Vector3 position in chargePositions)
+        {
+           // chickenBody.MovePosition()
+        }
+    }
+}
