a74ff7c0fd658f517ee2127749e4906779758e6c
taey1 <ybrgus@gmail.com>
2019-12-09 20:48:08 +0100

    obstacles for vertical

---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/VerticalObstacleCourse/SpikeEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spikes;
+    [SerializeField]
+    [Range(0.1f, 5f)]
+    private float spikesActiveTime;
+    [SerializeField]
+    [Range(0.1f, 5f)]
+    private float spikesResetTime;
+
+
+
+    void Start()
+    {
+        EventHandler.Instance.Register(EventHandler.EventType.StartMinigameEvent, StartMechanics);
+        StartMechanics(new StartMinigameEventInfo()); //Debug testing
+    }
+
+    private void StartMechanics(BaseEventInfo e)
+    {
+        StartCoroutine("ToggleSpikes");
+    }
+
+    IEnumerator ToggleSpikes()
+    {
+        spikes.SetActive(true);
+        yield return new WaitForSeconds(spikesActiveTime);
+        spikes.SetActive(false);
+        yield return new WaitForSeconds(spikesResetTime);
+        StartCoroutine("ToggleSpikes");
+    }
+
+}
---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/VerticalObstacleCourse/TrapDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoor : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 5f)]
+    private float trapDoorUpTime;
+    [SerializeField]
+    [Range(0.1f, 5f)]
+    private float trapDoorDownTime;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        EventHandler.Instance.Register(EventHandler.EventType.StartMinigameEvent, StartMechanics);
+        StartMechanics(new StartMinigameEventInfo()); //Debug testing
+    }
+    private void StartMechanics(BaseEventInfo e)
+    {
+        StartCoroutine("ToggleTrapDoor");
+    }
+
+    IEnumerator ToggleTrapDoor()
+    {
+        anim.SetTrigger("ActivateTrapDoor");
+        yield return new WaitForSeconds(trapDoorDownTime);
+        anim.SetTrigger("ResetTrapDoor");
+        yield return new WaitForSeconds(trapDoorUpTime);
+        StartCoroutine("ToggleTrapDoor");
+    }
+}
