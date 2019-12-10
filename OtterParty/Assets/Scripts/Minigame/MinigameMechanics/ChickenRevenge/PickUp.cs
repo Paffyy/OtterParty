9e61116f7e377453851ad2767551f03855b2b76c
taey1 <ybrgus@gmail.com>
2019-12-10 15:27:03 +0100

    PickUp stuff

---
+++ OtterParty/Assets/Scripts/Minigame/MinigameMechanics/ChickenRevenge/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 10)]
+    private int points;
+    public int Points { get { return points; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FirePickUpEvent(other.gameObject);
+        }
+    }
+
+    private void FirePickUpEvent(GameObject player)
+    {
+        PickUpEventInfo pei = new PickUpEventInfo(gameObject, player);
+        EventHandler.Instance.FireEvent(EventHandler.EventType.PickUpEvent, pei);
+    }
+}
