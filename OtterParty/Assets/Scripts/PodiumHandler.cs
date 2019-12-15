d2358f23cb2ecccfe4a11c68907794965f37225e
taey1 <ybrgus@gmail.com>
2019-12-15 22:08:00 +0100

    staging stuff

---
+++ OtterParty/Assets/Scripts/PodiumHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodiumHandler : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> podiumPrefabs;
+    [SerializeField]
+    private List<Transform> podiumPlacements;
+    [SerializeField]
+    private float showPlacementsDelay;
+
+    void Awake()
+    {
+        foreach (Transform item in gameObject.transform)
+        {
+            podiumPlacements.Add(item);
+        }
+    }
+
+    void Start()
+    {
+        EventHandler.Instance.Register(EventHandler.EventType.StartMinigameEvent, InitiateCeremony);
+    }
+
+    private void InitiateCeremony(BaseEventInfo e)
+    {
+        StartCoroutine("ShowPlacements");
+    }
+
+    IEnumerator ShowPlacements()
+    {
+        yield return new WaitForSeconds(showPlacementsDelay);
+        AssignPodiums();
+    }
+
+    private void AssignPodiums()
+    {
+        foreach (var player in GameController.Instance.Players)
+        {
+            int i = 0;
+            var podium = Instantiate(podiumPrefabs[i]);
+        }
+    }
+}
