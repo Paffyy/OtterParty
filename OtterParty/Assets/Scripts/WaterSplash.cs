1347ea1bd357504cd7770847ec633f9e888eb093
taey1 <ybrgus@gmail.com>
2020-01-03 14:44:40 +0100

    WaterSplash when player hits water

--- OtterParty/Assets/Scripts/WaterSplash.cs
+++ OtterParty/Assets/Scripts/WaterSplash.cs
@@ -8,13 +8,18 @@ public class WaterSplash : MonoBehaviour
     private AudioClip waterSplash;
     [SerializeField]
     private float waterSplashVolume;
+    [SerializeField]
+    private GameObject waterSplashEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            TransformEventInfo tei = new TransformEventInfo(other.gameObject.transform.position, waterSplashEffect.transform.rotation, waterSplashEffect);
+            EventHandler.Instance.FireEvent(EventHandler.EventType.ParticleEvent, tei);
             SoundEventInfo sei = new SoundEventInfo(waterSplash, waterSplashVolume, 1);
             EventHandler.Instance.FireEvent(EventHandler.EventType.SoundEvent, sei);
+            Debug.Log("HitWater");
         }
     }
 }
